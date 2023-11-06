620718d046279268a5604ea16c09d241d3fdde92
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2023-11-06 20:58:08 +0600

    add mongoDB

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const express = require('express')
 const app = express()
 let cors = require('cors')
-const dotenv=require('dotenv').config()
+const dotenv = require('dotenv').config()
+const jwt = require('jsonwebtoken');
+let cookieparser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 // {
 //   origin:['http://localhost:5173'],
 //   credentials:true
 // }
-// name:Assignment-11-Server
-// pass:Assignment-11-Server
-app.use(cors());
-app.use(express.json())
-// console.log(process.env.DB_USER)
+app.use(cors( {
+    origin:['http://localhost:5173'],
+    credentials:true
+  }));
+app.use(express.json());
+app.use(cookieparser())
 
-// const uri = "mongodb+srv://Assignment-11-Server:Assignment-11-Server@cluster0.dhtqvw7.mongodb.net/?retryWrites=true&w=majority";
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("value of token ", token)
+  if (!token) {
+    return res.status(401).send({ message: 'not authorigation' })
+  }
+  jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
+    // error
+    if (err) {
+      return res.status(401).send({ message: 'not authorigation' })
+    }
+    // decoded
+    console.log('value of', decoded)
+    req.user = decoded
+    next()
+
+  })
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dhtqvw7.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -45,27 +66,53 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/blog', async(req, res) => {
+    app.get('/blog', async (req, res) => {
       let quer = {}
       if (req.query.category) {
-        quer = {category: req.query.category }
+        quer = { category: req.query.category }
       }
-      const cursor=blogCollection.find(quer)
+      const cursor = blogCollection.find(quer)
       const result = await cursor.toArray()
       res.send(result)
     })
 
-    app.get('/blog/:id', async(req, res) => {
+    app.get('/blog/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await blogCollection.findOne(query)
       res.send(result)
     })
 
-    app.post('/wishlist',async(req,res)=>{
-      const data=req.body
-      const result=await wishlistCollection.insertOne(data)
+    app.post('/wishlist', async (req, res) => {
+      const data = req.body
+      const result = await wishlistCollection.insertOne(data)
       res.send(result)
+    })
+
+    app.post("/jwt",async(req,res)=>{
+      const data=req.body
+      const token=jwt.sign(data, process.env.JWT_TOKEN, {expiresIn: '1h'});
+      res
+      .cookie('token',token,{
+        httpOnly:true,
+        secure:false,
+        sameSite:'strict',
+      })
+      .send({success:true})
+    })
+
+    app.get('/wishlist',async(req,res)=>{
+      let quer={}
+      console.log('user:', req.user)
+      if(req.query?.email !== req.query?.email){
+        return res.status(404).send({message:'not accis'})
+      }
+      if(req.query.email){
+        quer={email: req.query.email}
+      }
+      const cursor=wishlistCollection.find(quer)
+      const result=await cursor.toArray()
+        res.send(result)
     })
 
     await client.db("admin").command({ ping: 1 });
